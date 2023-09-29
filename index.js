0f70c3af7e7bc1ee00ef885c6ccf985e5994cc0a
soyerv <valentin.soyer46@gmail.com>
2023-09-29 15:11:39 +0200

    feat(prompt) improving

--- index.js
+++ index.js
@@ -13,17 +13,18 @@ async function getResponse(promptValue) {
     model: "gpt-3.5-turbo-instruct",
     prompt: promptValue,
     max_tokens: 2000,
-    temperature: 0,
+    temperature: 1.5,
   });
 
-  console.log(response);
+  return response;
 }
 
 let prompt = "";
 
-fs.readFile('prompts/generate_character.txt', 'utf-8', (err, data) => {
+fs.readFile('prompts/generate_character.txt', 'utf-8', async (err, data) => {
   if (err) throw err;
-  console.log(data)
+  const char = await getResponse(data)
 
-})
-getResponse("I'm a Dungeon Master for a Dungeon & Dragons Campaign, and I want to integrate original and convincing Non playable Character (Stated as NPC) into my stories with an interresting background. Generate one of those  NPC, you are free to choose any Dungeon & Dragons race, class and statistics that meet the rule requirements. Once done, you'll display the NPC level and it's FORCE, CHARISMA, INTELLIGENCE, WISDOW, CONSTITUTION and DEXTERITY Statistics. After that, i want you to generate a story for this character that is relevant with its statistics. Write this as A .json file containing all the usefull fields to get those information");
+  console.log(char);
+});
+
